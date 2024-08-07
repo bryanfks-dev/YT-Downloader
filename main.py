1fe12a5ee58dc6588d4e09b8dc0800d9108b2975
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-08-07 12:42:09 +0700

    reformat project structure

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-import colors
+import lib
 import sys
 import os
-import utils
-import config
 
 # Try-catch import required external libs
 try:
     from pytubefix import YouTube, Stream
 except ImportError:
     print(
-        f"{colors.FAIL}[ERROR]{colors.ENDC} There are missing external libs. Please install using `pip install requirements.txt`"
+        f"{lib.colors.FAIL}[ERROR]{lib.colors.ENDC} There are missing external libs. Please install using `pip install requirements.txt`"
     )
 
 
@@ -24,14 +22,14 @@ def confirmRewriteFile(path: str) -> bool:
         # Ask the user if they want to rewrite the file
         ans: str = str.upper(
             input(
-                f"{colors.WARNING}[WARNING]{colors.ENDC} File with this name already exists. Do you want to rewrite the file? [Y/N]"
+                f"{lib.colors.WARNING}[WARNING]{lib.colors.ENDC} File with this name already exists. Do you want to rewrite the file? [Y/N]"
             )
         )
 
         # Check if the input is valid
         if [ans != "Y", ans != "N"]:
             print(
-                f"{colors.FAIL}[ERROR]{colors.ENDC} Invalid input. Please enter Y or N"
+                f"{lib.colors.FAIL}[ERROR]{lib.colors.ENDC} Invalid input. Please enter Y or N"
             )
 
         if ans == "Y":
@@ -51,18 +49,20 @@ def downloadStream(stream: Stream | None) -> None:
 
     try:
         # Download the video
-        print(f"{colors.OKCYAN}[INFO]{colors.ENDC} Downloading video {fileName}...")
+        print(
+            f"{lib.colors.OKCYAN}[INFO]{lib.colors.ENDC} Downloading video {fileName}..."
+        )
 
         stream.download(
-            output_path=config["OUTPUT_FILE"]["VIDEO"]["PATH"],
+            output_path=lib.config["OUTPUT_FILE"]["VIDEO"]["PATH"],
             filename=f"{fileName}.mp4",
         )
 
         print(
-            f"{colors.OKGREEN}[SUCCESS]{colors.ENDC} Video {fileName} downloaded successfully"
+            f"{lib.colors.OKGREEN}[SUCCESS]{lib.colors.ENDC} Video {fileName} downloaded successfully"
         )
     except:
-        print(f"{colors.FAIL}[ERROR]{colors.ENDC} Error downloading the video")
+        print(f"{lib.colors.FAIL}[ERROR]{lib.colors.ENDC} Error downloading the video")
 
 
 def downloadVideo(url: str = "") -> None:
@@ -73,10 +73,10 @@ def downloadVideo(url: str = "") -> None:
         # Create a YouTube object
         yt: YouTube = YouTube(url)
     except:
-        print(f"{colors.FAIL}[ERROR]{colors.ENDC} Connection error")
+        print(f"{lib.colors.FAIL}[ERROR]{lib.colors.ENDC} Connection error")
 
     # Get the highest resolution stream
-    stream = (
+    stream: Stream = (
         yt.streams.filter(progressive=True, file_extension="mp4")
         .order_by("resolution")
         .desc()
@@ -90,8 +90,8 @@ def sanitizeURL(url: str = "") -> str:
     """Function to sanitize the URL to a valid YouTube URL"""
 
     # Ensure the URL is not empty
-    if utils.strIsEmpty(url):
-        print(f"{colors.FAIL}[ERROR]{colors.ENDC} Given URL is empty")
+    if lib.utils.strIsEmpty(url):
+        print(f"{lib.colors.FAIL}[ERROR]{lib.colors.ENDC} Given URL is empty")
         sys.exit(1)
 
     return str.strip(url)
